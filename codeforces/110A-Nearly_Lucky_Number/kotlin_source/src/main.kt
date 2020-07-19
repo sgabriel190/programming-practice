b4a35820ffe9b37b1a491770e197fe248161b69e
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-07-19 21:12:18 +0300

    added 110A

--- codeforces/110A-Nearly_Lucky_Number/kotlin_source/src/main.kt
+++ codeforces/110A-Nearly_Lucky_Number/kotlin_source/src/main.kt
@@ -1,17 +1,11 @@
 fun readString():String = readLine()!!.toString()
 fun main(){
-    val searchedCharsList = listOf<Char>('7', '4')
     val input = readString()
-    for (index in 0..input.length ){
-        if(input[index] in searchedCharsList){
-            var foundCheck = false
-            val searchedChar = if(input[index]=='4') '7' else '4'
-            for (token in index+1..input.length){
-                if(input[token] !in searchedCharsList){
-                    index = token
-                    break
-                }
-            }
-        }
+    val matchingSequencesList = input.split("[^47]".toRegex()).filter { it != "" }
+    val numberOfLuckyNumbers = matchingSequencesList.joinToString(separator = "").length
+    if(numberOfLuckyNumbers.toString().matches("[47]+".toRegex())){
+        println("YES")
+    }else{
+        println("NO")
     }
 }
