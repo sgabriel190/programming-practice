dc92cf6831ac3ec22acd99a64bf262a69c24bd83
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-09-03 13:40:16 +0300

    Updated kotlin iterator pattern.

--- design-patterns/behavioral-patterns/kotlin/iterator/src/main/kotlin/main.kt
+++ design-patterns/behavioral-patterns/kotlin/iterator/src/main/kotlin/main.kt
@@ -4,6 +4,11 @@
  * representation (list, stack tree, etc).
  * -> The main idea of the iterator pattern is to extract the traversal behavior of a collection into a separate
  * object called an iterator.
+ * -> Use the Iterator pattern when your collection has a complex data structure under the hood, but you want
+ * to hide its complexity from clients (either for convenience or security reasons).
+ * -> Use the pattern to reduce duplication of the traversal code across your app.
+ * -> Use the Iterator when you want your code to be able to traverse different data structures or when types of
+ * these structures are unknown beforehand.
  */
 fun main(){
     val network: SocialNetwork = Facebook()
